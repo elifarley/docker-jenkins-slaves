cebfcff06190e7d9431054179bd8413e6c890314
Elifarley C <elifarley@users.noreply.github.com>
2016-04-01 11:05:11 -0300

    removed dependency on 'dpkg'
    Use 'ldd' instead

--- docker-jenkins-slave.sh
+++ docker-jenkins-slave.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 set -x
 IMAGE="quay.io/elifarley/openjdk8-sshd-devel:latest"
-LIBDEVMAPPER="$(dpkg -L libdevmapper1.02.1 | grep \.so)"
 docker pull "$IMAGE"
+
+DOCKER_LIBS="$(ldd $(which docker) | grep libdevmapper | cut -d' ' -f3)"
+MOUNT_DOCKER="\
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v $DOCKER_LIBS:$DOCKER_LIBS:ro \
+-v $(which docker):$(which docker):ro
+"
 
 exec docker run --name jenkins-slave-devel \
 -d --restart=always \
 -p 2222:22 \
--v /var/run/docker.sock:/var/run/docker.sock \
--v "$(which docker)":"$(which docker)":ro \
--v "$LIBDEVMAPPER":"$LIBDEVMAPPER":ro \
+$MOUNT_DOCKER \
 -v ~/data/id_rsa.pub:/app/ssh-config/authorized_keys:ro \
 -v ~/data/bitbucket-private-key:/app/ssh-config/id_rsa:ro \
 -v ~/data/jenkins-slave:/app/data \
