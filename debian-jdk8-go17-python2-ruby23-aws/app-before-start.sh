6d9b70a07edae05c205863d299920ee4bae6f3cc
Elifarley C <elifarley@users.noreply.github.com>
2017-01-23 14:17:05 -0200

    Fix command order

--- debian-jdk8-go17-python2-ruby23-aws/app-before-start.sh
+++ debian-jdk8-go17-python2-ruby23-aws/app-before-start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+for d in .gradle .docker; do
+  if test -d "$_home/$d"; then
+    chown "$_USER:$_USER" "$_home/$d"
+  fi
+done
+
 for d in .bundle .jenkins/cache/jars .gradle/caches .gradle/daemon .gradle/native .gradle/wrapper; do
   test -d /data/"$d" || {
     $sudo mkdir -p /data/"$d" || return
@@ -8,14 +14,8 @@ done
 
 for d in .bundle .jenkins .gradle/caches .gradle/daemon .gradle/native .gradle/wrapper; do
   test -L "$_home"/"$d" -a -d "$_home"/"$d" || {
-    rm -rf "$_home/$d" && $sudo ln -s /data/"$d" "$_home/$d"
+    rm -rf "$_home/$d" && $sudo ln -s /data/"$d" "$_home/$d" || return
   }
-done
-
-for d in .gradle .docker; do
-  if test -d "$_home/$d"; then
-    chown "$_USER:$_USER" "$_home/$d"
-  fi
 done
 
 echo "$_home:"; ls -Falk "$_home"
